e7a039f55d9a70ca00709a948d301fd44c7d5fbb
FabrizioMisseri <misserifabrizio@gmail.com>
2022-10-19 15:44:33 +0200

    generate random numbers on the screen

--- js/script.js
+++ js/script.js
@@ -6,3 +6,29 @@
 // * Pensate prima in italiano.
 // * Dividete in piccoli problemi la consegna.
 // * Individuate gli elementi di cui avete bisogno per realizzare il programma.
+
+
+// creo 5 numeri casuali
+const rndNumbers = generateRndNumbers1to100(5);
+console.log(rndNumbers);
+// prendo UL, e stampo in numeri a schermo
+const item = document.getElementById("item");
+for (let i = 0; i < rndNumbers.length; i++) {
+    const element = document.createElement("li");
+    element.classList.add("size3");
+    element.innerHTML = `${rndNumbers[i]}`;
+    item.append(element);
+}
+
+
+
+
+// FUNCTIONS
+function generateRndNumbers1to100(index) {
+    const array = [];
+    for (let i = 0; i < index; i++) {
+        let rndNumber = Math.floor(Math.random() * (100 - 1 + 1) + 1);
+        array.push(rndNumber);
+    }
+    return array;
+}
